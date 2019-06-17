2e97eba8ef07b76cb39a248d17ddd47723584de3
Jorge Luis <cokimaniac@gmail.com>
2019-06-16 20:28:14 -0400

    img source bug fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 function imageElement() {
+    let imgPath = window.location.href.split('/')
+    imgPath.pop()
     const component = document.createElement("div")
     const img = document.createElement("img")
-    img.src = "../assets/ids2-image.png"
+    img.src = imgPath.join('/')+'/assets/ids2-image.png'
     img.id = "img-signature"
     component.className = "imgContainer"
     component.appendChild(img)
