79d1c5cce6c365968f60fe9561d15b1edecb6201
Jorge Luis <cokimaniac@gmail.com>
2019-06-16 20:07:55 -0400

    src files fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function imageElement() {
     const component = document.createElement("div")
     const img = document.createElement("img")
-    img.src = "/assets/ids2-image.png"
+    img.src = "../assets/ids2-image.png"
     img.id = "img-signature"
     component.className = "imgContainer"
     component.appendChild(img)
