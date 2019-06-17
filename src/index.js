dfbee4f7ad98005d39c641d883ef4a623d1d3f3d
Jorge Luis <cokimaniac@gmail.com>
2019-06-16 20:34:39 -0400

    add a test event to image

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ function imageElement() {
     imgPath.pop()
     const component = document.createElement("div")
     const img = document.createElement("img")
-    img.src = imgPath.join('/')+'/assets/ids2-image.png'
+    img.src = `${imgPath.join('/')}/assets/ids2-image.png`
     img.id = "img-signature"
+    img.addEventListener("click", function(){
+        alert("¿Aquí se despliega el menú?")
+    })
     component.className = "imgContainer"
     component.appendChild(img)
     return component
